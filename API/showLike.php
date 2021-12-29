77077337d502d6a9ac9395c4222db56088ab45f1
vijaypalgautam <myselfie196@gmail.com>
2021-12-29 19:36:58 +0530

    fixing connection

--- API/showLike.php
+++ API/showLike.php
@@ -1,11 +1,11 @@
 <?php
+include 'config.php';
 header('Access-Control-Allow-Origin: *');
 
-$con = mysqli_connect("localhost","root","","ourselfie") or die("server error!");
 $userId = $_POST['userId'];
 
 $query="SELECT liker, liked FROM likes WHERE liker = '$userId' ORDER BY id";
-$result = mysqli_query($con, $query);
+$result = mysqli_query($conn, $query);
 
 while ($row=mysqli_fetch_assoc($result)) 
 {
